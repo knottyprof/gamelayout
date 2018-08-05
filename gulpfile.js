b621052f4e513301fe2d6f24e7917487ab3e9f13
John Buckley <john@knotty-works.com>
2018-08-05 16:20:59 -0400

    gulp fixes

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var sass    = require('gulp-sass');
 
 //Compile sass into css
 gulp.task('sass', function(){
-    return gulp.src(['node_modules/bootstrap/scss/bootsrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
     .pipe(sass())
     .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream());
@@ -12,7 +12,7 @@ gulp.task('sass', function(){
 
 //Move javascript files to src/js folder
 gulp.task('js', function(){
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/js/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
     .pipe(gulp.dest("src/js"))
     .pipe(browserSync.stream());
 });
